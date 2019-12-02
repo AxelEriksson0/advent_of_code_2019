1ef2774aa3cf9793af8bbda3639d52524805a11b
Axel Eriksson <axeleriksson0@gmail.com>
2019-12-02 09:49:25 +0100

    Day 2 part 2 done

--- day_2/day_2.mjs
+++ day_2/day_2.mjs
@@ -1,27 +1,45 @@
 import fs from 'fs'
 
 const input = fs.readFileSync('./day_2_input', 'utf8')
-const programCodes = input.toString().split(',').map(Number)
+const originalProgramCodes = input.toString().split(',').map(Number)
 
-programCodes[1] = 12
-programCodes[2] = 2
+const runProgram = (programCodes, noun, verb) => {
+  programCodes[1] = noun
+  programCodes[2] = verb
+  for (let i = 0; i < programCodes.length; i += 4) {
+    if (programCodes[i] === 99) {
+      break
+    }
 
-for (let i = 0; i < programCodes.length; i += 4) {
-  if (programCodes[i] === 99) {
-    break
+    const position1 = programCodes[i + 1]
+    const position2 = programCodes[i + 2]
+    const positionToChange = programCodes[i + 3]
+
+    if (programCodes[i] === 1) {
+      programCodes[positionToChange] = programCodes[position1] + programCodes[position2]
+    }
+
+    if (programCodes[i] === 2) {
+      programCodes[positionToChange] = programCodes[position1] * programCodes[position2]
+    }
   }
 
-  const position1 = programCodes[i + 1]
-  const position2 = programCodes[i + 2]
-  const positionToChange = programCodes[i + 3]
-
-  if (programCodes[i] === 1) {
-    programCodes[positionToChange] = programCodes[position1] + programCodes[position2]
-  }
-
-  if (programCodes[i] === 2) {
-    programCodes[positionToChange] = programCodes[position1] * programCodes[position2]
+  if (programCodes[0] === 19690720) {
+    console.log(`You found the solution!
+    ProgramCodes[0] = ${programCodes[0]}
+    Noun = ${noun}
+    Verb = ${verb}
+    `)
   }
 }
 
-console.log(programCodes)
+const nouns = Array.from(Array(10000).keys())
+const verbs = Array.from(Array(10000).keys())
+
+nouns.forEach(noun => {
+  verbs.forEach(verb => {
+    runProgram([...originalProgramCodes], noun, verb)
+  })
+})
+
+/* runProgram([...originalProgramCodes], 12, 2) */
