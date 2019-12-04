d2585f9c74cbdd0c1909214f315a633edb00b6cb
Axel Eriksson <axeleriksson0@gmail.com>
2019-12-04 13:15:11 +0100

    Day 4 complete

--- day_4/day_4.mjs
+++ day_4/day_4.mjs
@@ -10,8 +10,28 @@ for (let i = lowerRange; i <= upperRange; i++) {
   if (digits.join() !== sorted.join()) {
     continue
   }
-  if (digits[0] === digits[1] || digits[1] === digits[2] || digits[2] === digits[3] || digits[3] === digits[4] || digits[4] === digits[5]) {
+  if (digits[0] !== digits[1] && digits[1] !== digits[2] && digits[2] !== digits[3] && digits[3] !== digits[4] && digits[4] !== digits[5]) {
+    continue
+  }
+  if (digits[0] === digits[1] && digits[1] !== digits[2]) {
     possiblePasswords.push(digits)
+    continue
+  }
+  if (digits[1] === digits[2] && digits[0] !== digits[1] && digits[2] !== digits[3]) {
+    possiblePasswords.push(digits)
+    continue
+  }
+  if (digits[2] === digits[3] && digits[1] !== digits[2] && digits[3] !== digits[4]) {
+    possiblePasswords.push(digits)
+    continue
+  }
+  if (digits[3] === digits[4] && digits[2] !== digits[3] && digits[4] !== digits[5]) {
+    possiblePasswords.push(digits)
+    continue
+  }
+  if (digits[4] === digits[5] && digits[3] !== digits[4]) {
+    possiblePasswords.push(digits)
+    continue
   }
 }
 
